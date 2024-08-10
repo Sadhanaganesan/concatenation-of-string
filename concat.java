57fbdfe99bd49055fbc3cb39fb62ee7b666f7bd5
Sadhana <145459079+Sadhanaganesan@users.noreply.github.com>
2024-08-10 21:04:14 +0530

    concat.java

---
+++ concat.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+         for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
